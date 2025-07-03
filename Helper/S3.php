29baabf7121301daddad0a110a4ab963b413198a
Jason Sun <sunf@shinetechsoftware.com>
2025-07-03 11:47:39 +0800

    Update AWS regions

--- Helper/S3.php
+++ Helper/S3.php
@@ -27,20 +27,20 @@ class S3
                 'label' => 'US East (N. Virginia)'
             ],
             [
-                'value' => 'us-west-2',
-                'label' => 'US West (Oregon)'
+                'value' => 'us-east-2',
+                'label' => 'US East (Ohio)'
             ],
             [
                 'value' => 'us-west-1',
                 'label' => 'US West (N. California)'
             ],
             [
-                'value' => 'eu-west-1',
-                'label' => 'EU (Ireland)'
+                'value' => 'us-west-2',
+                'label' => 'US West (Oregon)'
             ],
             [
-                'value' => 'eu-central-1',
-                'label' => 'EU (Frankfurt)'
+                'value' => 'af-south-1',
+                'label' => 'Africa (Cape Town)'
             ],
             [
                 'value' => 'ap-east-1',
@@ -55,16 +55,60 @@ class S3
                 'label' => 'Asia Pacific (Singapore)'
             ],
             [
-                'value' => 'ap-northeast-1',
-                'label' => 'Asia Pacific (Tokyo)'
-            ],
-            [
                 'value' => 'ap-southeast-2',
                 'label' => 'Asia Pacific (Sydney)'
             ],
             [
+                'value' => 'ap-southeast-3',
+                'label' => 'Asia Pacific (Jakarta)'
+            ],
+            [
+                'value' => 'ap-northeast-1',
+                'label' => 'Asia Pacific (Tokyo)'
+            ],
+            [
                 'value' => 'ap-northeast-2',
                 'label' => 'Asia Pacific (Seoul)'
+            ],
+            [
+                'value' => 'ap-northeast-3',
+                'label' => 'Asia Pacific (Osaka)'
+            ],
+            [
+                'value' => 'ca-central-1',
+                'label' => 'Canada (Central)'
+            ],
+            [
+                'value' => 'eu-central-1',
+                'label' => 'EU (Frankfurt)'
+            ],
+            [
+                'value' => 'eu-west-1',
+                'label' => 'EU (Ireland)'
+            ],
+            [
+                'value' => 'eu-west-2',
+                'label' => 'Europe (London)'
+            ],
+            [
+                'value' => 'eu-west-3',
+                'label' => 'Europe (Paris)'
+            ],
+            [
+                'value' => 'eu-south-1',
+                'label' => 'Europe (Milan)'
+            ],
+            [
+                'value' => 'eu-north-1',
+                'label' => 'Europe (Stockholm)'
+            ],
+            [
+                'value' => 'me-south-1',
+                'label' => 'Middle East (Bahrain)'
+            ],
+            [
+                'value' => 'me-central-1',
+                'label' => 'Middle East (UAE)'
             ],
             [
                 'value' => 'sa-east-1',
